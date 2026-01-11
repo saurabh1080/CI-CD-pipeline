8ab3b25ee19bb834ead1bd5e114548869d2c9bde
Saurabh <saurabhdubey1080@gmail.com>
2026-01-11 18:50:57 +0530

    CI CD pipeline tutorial

---
+++ src/main/java/com/pm/githubcicd/GithubCicdApplication.java
@@ -0,0 +1,27 @@
+package com.pm.githubcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class GithubCicdApplication {
+
+    @GetMapping("/welcome")
+    public String Welcome(){
+       return "Welcome to CI/CD";
+    }
+
+//    echo "# CI-CD-pipeline" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/saurabh1080/CI-CD-pipeline.git
+//    git push -u origin main
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdApplication.class, args);
+    }
+
+}
